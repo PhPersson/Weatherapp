ef76043e52721d27eb7bce4954a030d656c8eb6c
Philip Persson <51906227+PhPersson@users.noreply.github.com>
2023-06-15 22:19:25 +0200

    Implemeted a working weatherpage that fetch from the api. Only a hardcoded city right now

--- lib/about.dart
+++ lib/about.dart
@@ -12,32 +12,39 @@ class _AboutPageState extends State<AboutPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-          child: Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Project Weather',
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
+    return Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [Colors.blue[300]!, Colors.green[400]!])),
+          child: const Center(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Project Weather',
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                SizedBox(height: 16.0),
-                Text(
-                  'This is an weather app thats developed as a project for the course IDV535 using Flutter and the OpenWeatherMap API.',
-                  style: TextStyle(fontSize: 18.0),
-                  textAlign: TextAlign.center,
-                ),
-                Text(
-                  'Developed by Philip Persson \n phpersson@github',
-                  style: TextStyle(fontSize: 18.0, fontStyle: FontStyle.italic),
-                  textAlign: TextAlign.center,
-                ),
-              ],
+                  SizedBox(height: 16.0),
+                  Text(
+                    'This is an weather app thats developed as a project for the course IDV535 using Flutter and the OpenWeatherMap API.',
+                    style: TextStyle(fontSize: 18.0),
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(
+                    'Developed by Philip Persson \n phpersson@github',
+                    style: TextStyle(fontSize: 18.0, fontStyle: FontStyle.italic),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
