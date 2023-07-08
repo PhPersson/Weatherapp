f14df1717b2fa8abcde7182dfb1fb1df532ef681
Philip Persson <51906227+PhPersson@users.noreply.github.com>
2023-07-08 23:09:06 +0200

    Formated forecast_page.dart

--- lib/forecast_page.dart
+++ lib/forecast_page.dart
@@ -54,59 +54,63 @@ class _ForecastPageState extends State<ForecastPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              cityName,
-              style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                cityName,
+                style:
+                    const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: forecastList.length,
-              itemBuilder: (context, index) {
-                var forecast = forecastList[index];
-                return Column(
-                  children: [
-                    _ForeCast(
-                      text: '${forecast['dt_txt']}',
-                      textStyle: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    if (forecast['weather'] != null)
+            Expanded(
+              child: ListView.builder(
+                itemCount: forecastList.length,
+                itemBuilder: (context, index) {
+                  var forecast = forecastList[index];
+                  return Column(
+                    children: [
+                      _ForeCast(
+                        text: '${forecast['dt_txt']}',
+                        textStyle: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      if (forecast['weather'] != null)
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            _ForeCast(
+                              text: '${forecast['main']['temp']}°C',
+                              textStyle:
+                                  const TextStyle(fontWeight: FontWeight.bold),
+                              icon:
+                                  _getWeatherIcon(forecast['weather'][0]['id']),
+                            ),
+                            _ForeCast(
+                              text: '${forecast['weather'][0]['description']}',
+                              textStyle:
+                                  const TextStyle(fontStyle: FontStyle.italic),
+                            ),
+                          ],
+                        ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           _ForeCast(
-                            text: '${forecast['main']['temp']}°C',
-                            textStyle:
-                                const TextStyle(fontWeight: FontWeight.bold),
-                            icon: _getWeatherIcon(forecast['weather'][0]['id']),
-                          ),
-                          _ForeCast(
-                            text: '${forecast['weather'][0]['description']}',
-                            textStyle:
-                                const TextStyle(fontStyle: FontStyle.italic),
+                            text: 'Wind: ${forecast['wind']['speed']}m/s',
+                            icon: Icons.air,
                           ),
                         ],
                       ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        _ForeCast(
-                          text: 'Wind: ${forecast['wind']['speed']}m/s',
-                          icon: Icons.air,
-                        ),
-                      ],
-                    ),
-                  ],
-                );
-              },
+                    ],
+                  );
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
